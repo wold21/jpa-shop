51b4c7b2394545c8d73e2cb238f6313ecfddaa35
hyuk.kim <wold2180@gmail.com>
2022-04-17 16:05:02 +0900

    멤버 엔티티 작성

--- src/main/java/com/jpabook/jpashop/domain/Member.java
+++ src/main/java/com/jpabook/jpashop/domain/Member.java
@@ -6,20 +6,22 @@ import org.apache.tomcat.jni.Address;
 
 import javax.persistence.*;
 import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter @Setter
 public class Member {
 
     @Id @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
 
-    private String username;
+    private String name;
 
-//    @Embedded
-//    private Address address;
-//
-//    @OneToMany(mappedBy = "member")
-//    private List<Order> orders = new ArrayList<>();
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<>();
 
 }
