2f8f9c8449a58ca980bbb870afd06b65a6a5816f
hyuk.kim <wold2180@gmail.com>
2022-04-19 20:59:10 +0900

    멤버 서비스 테스트 생성

---
+++ src/test/java/com/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,25 @@
+package com.jpabook.jpashop.service;
+
+import com.jpabook.jpashop.repository.MemberRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    
+}
